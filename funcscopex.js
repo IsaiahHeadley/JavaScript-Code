f7a85cc7ba0aa46867e300e3deb4446d2ca2853c
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2020-12-28 09:59:02 -0500

    Add files via upload

---
+++ funcscopex.js
@@ -0,0 +1,11 @@
+//scope example, that is immediately invoked
+
+var firstName = 'Jane';
+
+(function(lastName) {
+var  firstName = 'John';
+console.log(firstName);
+console.log(lastName);
+}('Doe'));
+
+console.log(firstName);
